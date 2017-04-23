d2c85b43f45f25ed76ddb8ea00a0804a8ba9112f
Starlynne <lewis.start0905@gmail.com>
2017-04-22 21:45:45 -0700

    Scripts not commecting

---
+++ public/js/scripts.js
@@ -0,0 +1,39 @@
+// click_count = 1
+// assignPiece = function(param) {
+//   if (click_count === 1){
+//     $(param).addClass("queen")
+//   }
+// }
+// first click
+// if click count === 1
+// if td has class black, add class white queen
+// else add class black queen
+//text input to queen form field
+
+// second click
+// if click count === 2
+// if td has class black, add class white player
+// else add class black player
+//text input to player form field
+
+// third click
+// if click count % 2 === 0
+// remove all queen classes, add correct queen class
+//text input to queen form field
+
+// fourth click
+// if click count % 2 !== 0
+// remove all player classes, add correct player class
+//text input to player form field
+
+// further clicks repeat steps 4 and five
+//After submit, how do we leave classes but reset count??
+//just reset count var to 1
+//if the fields are used instead, how to add correct classes?
+
+
+// $(document).ready(function(){
+//   $('form').submit(function(){
+//       console.log("hello")
+//   });
+// });
